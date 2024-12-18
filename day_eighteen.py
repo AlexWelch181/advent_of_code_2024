ff0efba5e6beed72f62b90477edbf0a81ce8f228
AlexWelch181 <55792490+AlexWelch181@users.noreply.github.com>
2024-12-18 20:37:06 +0000

    accidentally looked at seen instead of the path for collisions

--- day_eighteen.py
+++ day_eighteen.py
@@ -12,18 +12,20 @@ grid = [['#' if (x,y) in cords[:i] else '.' for x in range(71)] for y in range(7
 
 ROWS, COLS = len(grid), len(grid[0])
 seen = set()
-
+finalPath = []
 def bfs():
-    q = deque([(0,0,0)])
+    q = deque([(0,0,0, [(0,0)])])
     while q:
-        s, r, c = q.popleft()
+        s, r, c, path = q.popleft()
         for dr, dc in [(-1,0),(0,1),(1,0),(0,-1)]:
             rr, cc = r+dr, c+dc
             if 0<=rr<ROWS and 0<=cc<COLS and grid[rr][cc] != '#' and (rr,cc) not in seen:
                 if (rr,cc) == (ROWS-1, COLS-1):
+                    global finalPath 
+                    finalPath = path
                     return s+1
                 seen.add((rr,cc))
-                q.append((s+1, rr, cc))
+                q.append((s+1, rr, cc, path+[(rr,cc)]))
     return None
 
 print(bfs())
@@ -31,8 +33,9 @@ print(bfs())
 for v in range(i, len(cords)):
     x, y = cords[v]
     grid[y][x] = '#'
-    if (y, x) in seen:
+    if (y, x) in finalPath:
         seen = set()
+        finalPath = []
         if bfs() is None:
             print('{0},{1}'.format(x,y))
             break
